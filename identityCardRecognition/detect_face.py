a3d08a2fad053455706a8bc8ea96e43e6317c3c6
musimab <mustafabuyuk42@hotmail.com>
2022-10-04 23:05:08 +0300

    face detector improved by adding abstraction

--- identityCardRecognition/detect_face.py
+++ identityCardRecognition/detect_face.py
@@ -14,10 +14,30 @@ class FaceDetector(ABC):
     @abstractmethod
     def findFace(self,img):
         pass
-
-    @abstractmethod
+    
     def rotate_bound(self,image, angle):
-        pass
+        # grab the dimensions of the image and then determine the
+        # centre
+        (h, w) = image.shape[:2]
+        (cX, cY) = (w // 2, h // 2)
+
+        # grab the rotation matrix (applying the negative of the
+        # angle to rotate clockwise), then grab the sine and cosine
+        # (i.e., the rotation components of the matrix)
+        M = cv2.getRotationMatrix2D((cX, cY), angle, 1.0)
+        cos = np.abs(M[0, 0])
+        sin = np.abs(M[0, 1])
+
+        # compute the new bounding dimensions of the image
+        nW = int((h * sin) + (w * cos))
+        nH = int((h * cos) + (w * sin))
+
+        # adjust the rotation matrix to take into account translation
+        M[0, 2] += (nW / 2) - cX
+        M[1, 2] += (nH / 2) - cY
+
+        # perform the actual rotation and return the image
+        return cv2.warpAffine(image, M, (nW, nH))
 
     @abstractmethod
     def cropFace(self, image):
@@ -65,34 +85,13 @@ class DlibFaceDetector(FaceDetector):
 
         return image[y1 :y2, x1:x2]
     
-    
-    def rotate_bound(self, image, angle):
-        # grab the dimensions of the image and then determine the
-        # centre
-        (h, w) = image.shape[:2]
-        (cX, cY) = (w // 2, h // 2)
-
-        # grab the rotation matrix (applying the negative of the
-        # angle to rotate clockwise), then grab the sine and cosine
-        # (i.e., the rotation components of the matrix)
-        M = cv2.getRotationMatrix2D((cX, cY), angle, 1.0)
-        cos = np.abs(M[0, 0])
-        sin = np.abs(M[0, 1])
-
-        # compute the new bounding dimensions of the image
-        nW = int((h * sin) + (w * cos))
-        nH = int((h * cos) + (w * sin))
-
-        # adjust the rotation matrix to take into account translation
-        M[0, 2] += (nW / 2) - cX
-        M[1, 2] += (nH / 2) - cY
-
-        # perform the actual rotation and return the image
-        return cv2.warpAffine(image, M, (nW, nH))
-
 
 class SsdFaceDetector(FaceDetector):
-    
+    def __init__(self) -> None:
+        super().__init__()
+        modelFile = "identityCardRecognition/model/res10_300x300_ssd_iter_140000.caffemodel"
+        configFile = "identityCardRecognition/model/deploy.prototxt.txt"
+        self.FaceNet = cv2.dnn.readNetFromCaffe(configFile, modelFile)
     
     def changeOrientationUntilFaceFound(self,image, rot_interval):
         """
@@ -120,17 +119,12 @@ class SsdFaceDetector(FaceDetector):
     
     def findFace(self,img):
         
-        modelFile = "identityCardRecognition/model/res10_300x300_ssd_iter_140000.caffemodel"
-        configFile = "identityCardRecognition/model/deploy.prototxt.txt"
-        FaceNet = cv2.dnn.readNetFromCaffe(configFile, modelFile)
-
         h, w = img.shape[:2]
         blob = cv2.dnn.blobFromImage(cv2.resize(img, (300, 300)), 1.0,
         (300, 300), (104.0, 117.0, 123.0))
         
-        FaceNet.setInput(blob)
-        faces = FaceNet.forward()
-        
+        self.FaceNet.setInput(blob)
+        faces = self.FaceNet.forward()
         
         for i in range(faces.shape[2]):
             confidence = faces[0, 0, i, 2]
@@ -142,35 +136,33 @@ class SsdFaceDetector(FaceDetector):
                 return confidence
             return 0
     
-    def cropFace(self, image):
-        pass
+    def cropFace(self, img):
+        h, w = img.shape[:2]
+        blob = cv2.dnn.blobFromImage(cv2.resize(img, (300, 300)), 1.0,
+        (300, 300), (104.0, 117.0, 123.0))
+        
+        self.FaceNet.setInput(blob)
+        faces = self.FaceNet.forward()
+        
+        for i in range(faces.shape[2]):
+            confidence = faces[0, 0, i, 2]
+            if confidence > 0.6:
+                # compute the (x, y)-coordinates of the bounding box for the object
+                box = faces[0, 0, i, 3:7] * np.array([w, h, w, h])
+                (x1, y1, x2, y2) = box.astype("int")
+                
+                #print("Confidence:", confidence)
+            
+            return img[y1:y2, x1:x2]
+        
+        return None
     
     
-    def rotate_bound(self,image, angle):
-        # grab the dimensions of the image and then determine the
-        # centre
-        (h, w) = image.shape[:2]
-        (cX, cY) = (w // 2, h // 2)
-
-        # grab the rotation matrix (applying the negative of the
-        # angle to rotate clockwise), then grab the sine and cosine
-        # (i.e., the rotation components of the matrix)
-        M = cv2.getRotationMatrix2D((cX, cY), angle, 1.0)
-        cos = np.abs(M[0, 0])
-        sin = np.abs(M[0, 1])
-
-        # compute the new bounding dimensions of the image
-        nW = int((h * sin) + (w * cos))
-        nH = int((h * cos) + (w * sin))
-
-        # adjust the rotation matrix to take into account translation
-        M[0, 2] += (nW / 2) - cX
-        M[1, 2] += (nH / 2) - cY
-
-        # perform the actual rotation and return the image
-        return cv2.warpAffine(image, M, (nW, nH))
-
 class HaarFaceDetector(FaceDetector):
+    def __init__(self) -> None:
+        super().__init__()
+        self.  face_cascade = cv2.CascadeClassifier('identityCardRecognition/model/haarcascade_frontalface_default.xml')
+
     
     def changeOrientationUntilFaceFound(self,image, rot_interval):
         img = image.copy()
@@ -189,8 +181,7 @@ class HaarFaceDetector(FaceDetector):
     def findFace(self,img):
         
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        face_cascade = cv2.CascadeClassifier('identityCardRecognition/model/haarcascade_frontalface_default.xml')
-        faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+        faces = self.face_cascade.detectMultiScale(gray, 1.3, 5)
         num_of_faces = len(faces)
             
         if(num_of_faces ):
@@ -199,34 +190,18 @@ class HaarFaceDetector(FaceDetector):
         
         return False
     
-    def cropFace(self, image):
-        pass
+    def cropFace(self, img):
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        rects = self.face_cascade.detectMultiScale(gray, 1.3, 5)
+        if(not len(rects)):
+            return 
+        (x, y, w, h)  = rects
+        
+        return img[y:y+h, x:x+w]
         
             
         
-    def rotate_bound(self,image, angle):
-        # grab the dimensions of the image and then determine the
-        # centre
-        (h, w) = image.shape[:2]
-        (cX, cY) = (w // 2, h // 2)
 
-        # grab the rotation matrix (applying the negative of the
-        # angle to rotate clockwise), then grab the sine and cosine
-        # (i.e., the rotation components of the matrix)
-        M = cv2.getRotationMatrix2D((cX, cY), angle, 1.0)
-        cos = np.abs(M[0, 0])
-        sin = np.abs(M[0, 1])
-
-        # compute the new bounding dimensions of the image
-        nW = int((h * sin) + (w * cos))
-        nH = int((h * cos) + (w * sin))
-
-        # adjust the rotation matrix to take into account translation
-        M[0, 2] += (nW / 2) - cX
-        M[1, 2] += (nH / 2) - cY
-
-        # perform the actual rotation and return the image
-        return cv2.warpAffine(image, M, (nW, nH))
 
 class FaceFactory(ABC):
     
